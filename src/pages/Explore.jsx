03cbd9b299938ba6eaa8206c749c04191d4a1c11
Renato Brant <renatobrant@gmail.com>
2026-04-22 23:11:05 +0000

    File changes

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -275,7 +275,7 @@ export default function Explore() {
       <VoxylHeader title="Explorar" subtitle="Descubra podcasts e playlists" />
 
       {/* Tabs */}
-      <div className="flex gap-2 px-4 flex-wrap">
+      <div className="flex gap-2 px-4 overflow-x-auto">
         {TABS.map(({ key, label, icon: TabIcon }) => (
           <button
             key={key}
